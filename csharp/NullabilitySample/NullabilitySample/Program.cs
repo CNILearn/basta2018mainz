5ba82397ddc00a3791eaca073ebbdae0acc9f825
Christian Nagel <christian@christiannagel.com>
2018-09-22 18:04:10 +0200

    nullability

---
+++ csharp/NullabilitySample/NullabilitySample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[module:NonNullTypes]
+
+namespace NullabilitySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Book("Professional C# 8");
+
+            if (b1.Publisher != null)
+            {
+                Console.WriteLine(b1.Publisher.ToLower());
+            }
+            Console.WriteLine(b1.Publisher?.ToLower());
+
+            var b2 = new LegacyBook("Professional C# 6");
+            Console.WriteLine(b2.Publisher.ToLower());
+        }
+    }
+}
