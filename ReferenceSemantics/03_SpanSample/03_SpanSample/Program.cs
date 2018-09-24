26140e67727c4bd0c8b2de6ef6b25ea9359e925b
Christian Nagel <christian@christiannagel.com>
2018-09-24 16:20:19 +0200

    cleanup span samples

--- ReferenceSemantics/03_SpanSample/03_SpanSample/Program.cs
+++ ReferenceSemantics/03_SpanSample/03_SpanSample/Program.cs
@@ -9,20 +9,42 @@ namespace _03_SpanSample
         {
             var x = new StackOnlyType();
             string s = x.ToString();
+            Console.WriteLine(s);
+            ArraySample();
+            StringSample();
         }
 
         private static void StringSample()
         {
+            Console.WriteLine(nameof(StringSample));
             var str1 = "The quick brown fox jumped over the lazy dogs";
+
+            // create a span from a string
             ReadOnlySpan<char> span1 = str1.AsSpan();
 
+            // create a span referencing a buffer large enough to hold a new string
+            Span<char> span2 = new Span<char>(new char[46]);
+
+            // copy the first string to the second span - if the buffer is large enough
+            span1.TryCopyTo(span2);
+            Console.WriteLine(span2.ToString());
+
+            // get the index of a string in the span
+            int ix = span2.IndexOf("lazy");
+
+            // replace a string within the string
+            ReadOnlySpan<char> replaceSpan = "slow".AsSpan();
+            Span<char> toReplace = span2.Slice(ix, 4);
+            replaceSpan.TryCopyTo(toReplace);
+
+            Console.WriteLine(span2.ToString());
         }
 
         private static void ArraySample()
         {
+            Console.WriteLine(nameof(ArraySample));
             int[] arr1 = Enumerable.Range(1, 100).ToArray();
             var span1 = arr1.AsSpan();
-            var s1 = span1.ToString();
             var slice1 = span1.Slice(0, 10);
             for (int i = 0; i < slice1.Length; i++)
             {
@@ -32,7 +54,7 @@ namespace _03_SpanSample
             {
                 Console.WriteLine(item);
             }
-           
+            Console.WriteLine();
         }
     }
 }
--- ReferenceSemantics/03_SpanSample/03_SpanSample/StackOnlyType.cs
+++ ReferenceSemantics/03_SpanSample/03_SpanSample/StackOnlyType.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace _03_SpanSample
+﻿namespace _03_SpanSample
 {
     ref struct StackOnlyType
     {
